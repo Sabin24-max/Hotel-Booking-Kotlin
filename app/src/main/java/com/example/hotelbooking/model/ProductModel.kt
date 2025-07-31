01bafa30a971f0c81edb603091b6b8e483770ae8
Sabin24-max <sabinshrestha0624@gmail.com>
2025-07-31 08:31:02 +0545

    Productmodelupdates

--- app/src/main/java/com/example/hotelbooking/model/ProductModel.kt
+++ app/src/main/java/com/example/hotelbooking/model/ProductModel.kt
@@ -1,8 +1,8 @@
 package com.example.hotelbooking.model
 
 data class ProductModel(
-    var productId: String = "",
-    var name: String = "",
+    var productId: String,
+    var name: String,
     var price: Double = 0.0,
     var description: String = "",
     var imageUrl: String = ""
