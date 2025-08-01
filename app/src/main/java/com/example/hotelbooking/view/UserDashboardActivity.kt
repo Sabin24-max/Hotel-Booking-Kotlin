3c275a89b525d7f1d34825f92acab24c9cf269db
Sabin24-max <sabinshrestha0624@gmail.com>
2025-08-01 19:51:12 +0545

    dashboard

--- app/src/main/java/com/example/hotelbooking/view/UserDashboardActivity.kt
+++ app/src/main/java/com/example/hotelbooking/view/UserDashboardActivity.kt
@@ -3,13 +3,11 @@ package com.example.hotelbooking.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.example.hotelbooking.component.UserHomeWithBottomNav
 
 class UserDashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             UserHomeWithBottomNav()
         }
