08d4c6af06103dab194a0008ee74517899b98b91
Sabin24-max <sabinshrestha0624@gmail.com>
2025-08-01 19:47:47 +0545

    admin

--- app/src/main/java/com/example/hotelbooking/view/admin/AdminDashboardActivity.kt
+++ app/src/main/java/com/example/hotelbooking/view/admin/AdminDashboardActivity.kt
@@ -3,14 +3,11 @@ package com.example.hotelbooking.view.admin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.example.hotelbooking.component.AdminDashboardMain
-
 
 class AdminDashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             AdminDashboardMain()
         }
