306e0165cf23773b434877793b6f56ecb2b3cec4
Sabin24-max <sabinshrestha0624@gmail.com>
2025-08-01 19:56:48 +0545

    updated

--- app/src/main/java/com/example/hotelbooking/repository/UserRepository.kt
+++ app/src/main/java/com/example/hotelbooking/repository/UserRepository.kt
@@ -4,20 +4,12 @@ import com.example.hotelbooking.model.UserModel
 import com.google.firebase.auth.FirebaseUser
 
 interface UserRepository {
-
     fun login(email: String, password: String, callback: (Boolean, String) -> Unit)
-
     fun register(email: String, password: String, callback: (Boolean, String, String) -> Unit)
-
     fun forgetPassword(email: String, callback: (Boolean, String) -> Unit)
-
     fun updateProfile(userID: String, data: MutableMap<String, Any?>, callback: (Boolean, String) -> Unit)
-
     fun getCurrentUser(): FirebaseUser?
-
     fun addUserToDatabase(userID: String, model: UserModel, callback: (Boolean, String) -> Unit)
-
     fun getUserByID(userID: String, callback: (UserModel?, Boolean, String) -> Unit)
-
     fun logout(callback: (Boolean, String) -> Unit)
 }
